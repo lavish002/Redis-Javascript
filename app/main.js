51518bb5e58ebdc561149ee56f84aa0df38a4b86
Lavish Garg <lgarg_be19@thapar.edu>
2024-11-21 02:50:10 +0530

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -6,7 +6,23 @@ console.log("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
   // Handle connection
-  connection.write('+PONG\r\n');
+  connection.addListener('data', (data) => {
+    const dataStr = data.toString();
+    
+    if (dataStr === '*1\r\n$4\r\nPING\r\n') {
+      connection.write('+PONG\r\n');
+    }
+  })
+
+  process.on('exit', () => {
+    server.close();
+  })
+  
+
+  connection.on("close", () => {
+    connection.end();
+  })
+
 });
 
 server.listen(6379, () => console.log("127.0.0.1:6379"));
